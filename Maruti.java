d6d983f16aaee49910f8ed09866a5dc0a5971afb
DeekshantBhardwaj <93893639+DeekshantBhardwaj@users.noreply.github.com>
2022-09-22 12:14:30 +0000

    Create Maruti.java

---
+++ Maruti.java
@@ -0,0 +1,16 @@
+package Corejava;
+class Maruti extends Car{
+	//Default contructor of maruti cars
+	   public Maruti()
+	   {
+		System.out.println("Class Maruti");
+	   }
+	   public void brand()
+	   {
+		System.out.println("Brand: Maruti");
+	   }
+	   public void speed()
+	   {
+		System.out.println("Max: 90Kmph");
+	   }
+	}
