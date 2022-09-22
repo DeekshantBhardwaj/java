0ea3622d5ab13eab636bcc6f977d08baa700bff6
DeekshantBhardwaj <93893639+DeekshantBhardwaj@users.noreply.github.com>
2022-09-22 12:13:27 +0000

    Create car.java

---
+++ car.java
@@ -0,0 +1,15 @@
+package Corejava;
+
+//When a class extends a class, which extends another class then this is called multilevel inheritance.
+
+class Car{
+//default contructor of car
+ public Car()
+ {
+	System.out.println("Class Car");
+ }
+ public void vehicleType()
+ {
+	System.out.println("Vehicle Type: Car");
+ }
+}
