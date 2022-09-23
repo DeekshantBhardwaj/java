f4dbf6f79d38150ff2637f795c86d8489b106736
DeekshantBhardwaj <93893639+DeekshantBhardwaj@users.noreply.github.com>
2022-09-23 06:28:39 +0000

    Create Teacher.java

---
+++ Teacher.java
@@ -0,0 +1,20 @@
+class Teacher
+{
+  public void teach()
+  {
+   system.out.println("Teaching Students");
+  }
+}
+class Student Extends Teacher
+{
+  public void listen()
+  {
+    system.out.println("Listen Teachere");
+  }
+}
+public static void main(String args[])
+{
+  Student s = new student();
+  s.teach;
+  s.listen;
+}
