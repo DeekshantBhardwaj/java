0ed0161d64aa124c8987a69ece719606a926f696
DeekshantBhardwaj <93893639+DeekshantBhardwaj@users.noreply.github.com>
2022-09-26 13:46:46 +0000

    Update inhertance1.java

--- HandsOwnprojects/inhertance1.java
+++ HandsOwnprojects/inhertance1.java
@@ -1,1 +1,32 @@
+class Animal
+{
+  void eat()
+  {
+    System.out.println("I am a omnivorous")
+  }
+}
 
+class mammel extends Animal
+{
+    void  nature()
+  {
+    System.out.println("I am a Mammel")
+   }
+}
+class Dog extends Mammal 
+    {
+        void sound()
+        {
+          System.out.println("I am Bark")
+        }
+    }
+class main()
+{
+  public static void main(String[] args)
+  {
+    Dog d = new Dog();
+    d.eat();
+    d.nature();
+    d.sound();
+  }
+}
