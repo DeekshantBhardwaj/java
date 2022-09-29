7a7294ee44bca7013484717a536baae8992e12f6
DeekshantBhardwaj <93893639+DeekshantBhardwaj@users.noreply.github.com>
2022-09-29 13:58:54 +0000

    Create versioncheck.java

---
+++ HandsOwnprojects/Thrusday1/versioncheck.java
@@ -0,0 +1,8 @@
+    class Java  
+    {  
+    public static void main(String args[])  
+    {  
+    //determines JRE version  
+    System.out.println("Java Version is: "+System.getProperty("java.version"));  
+    }  
+    }  
