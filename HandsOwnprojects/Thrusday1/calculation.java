f619c2ff90162ceb4cd1aa0df118ce96c6d76d98
DeekshantBhardwaj <93893639+DeekshantBhardwaj@users.noreply.github.com>
2022-09-29 16:03:21 +0000

    Create calculation.java

---
+++ HandsOwnprojects/Thrusday1/calculation.java
@@ -0,0 +1,18 @@
+/*  Ques3:Create a program for calculation.read three values from the user for operation
+first and second values as operands.
+third value as operator.
+if user press1 => print addition of first and second
+if user press2 => print subtraction of first and second
+if user press3 => print multiplication of first and second
+if user press4 => print division of first and second    */
+
+import utils.Scanner
+class Calculation()
+{
+  public static void main(String[] args)
+  {
+    Scanner sc =new Scanner(System.in);
+    int a= sc.nextInt();
+    System.out.println()
+      int b= sc.nextint();
+    System.out.next
