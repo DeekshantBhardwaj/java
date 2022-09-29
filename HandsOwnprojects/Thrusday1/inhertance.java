c8b38778c2a618ca807df0d20bd80505f464d68d
DeekshantBhardwaj <93893639+DeekshantBhardwaj@users.noreply.github.com>
2022-09-29 13:20:34 +0000

    Create inhertance.java

---
+++ HandsOwnprojects/Thrusday1/inhertance.java
@@ -0,0 +1,28 @@
+class parent
+  //making a class name parent
+{
+  void sing()//method making
+  {
+    System.out.println("i am singing")
+    }
+}
+class child extends parent()//extends the class with super class
+{
+  void speak()//another method
+  {
+    System.out.println("i am speak")
+    }
+  void dance()//method
+  {
+    System.out.println("i am dancing")
+    }
+  class main()//main class with calling all the objects
+  {
+    public static void main(String[] args)
+      //creating a object or calling the above method
+      main d=new main();
+    d.sing();//calling the above method
+    d.speak();//calling the above method
+    d.dance();//calling the above method
+  }
+}               
