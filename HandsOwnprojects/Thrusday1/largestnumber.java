bee5b90ac4aa0f63e6236c4256d3334c19029c0a
DeekshantBhardwaj <93893639+DeekshantBhardwaj@users.noreply.github.com>
2022-09-30 09:16:37 +0000

    Update largestnumber.java

--- HandsOwnprojects/Thrusday1/largestnumber.java
+++ HandsOwnprojects/Thrusday1/largestnumber.java
@@ -7,6 +7,7 @@ Date:29 sept 2022
 
 
 //Creating a programme to find largest of two numbers...........
+//Creating a programme to find largest of two numbers...........
 import java.util.Scanner;//this is use because the inbuilt feature in jasa to take output from the user
 class Sum //create a main class
 {
@@ -15,20 +16,20 @@ class Sum //create a main class
      Scanner sc= new Scanner(System.in);//creating caaling class object here
     
     System.out.println("enter first number");//enter the first input
-		int num1 = sc.nextInt();
+		int number1= sc.nextInt();
     
     
-    System.out.println("enter second number")//Second input from the user
-    int num2 = sc.nextInt();
+    System.out.println("enter second number");//Second input from the user
+    int number2  =sc.nextInt();
     
-    if(number1<number2)//conditional loop is implement where you can see that opoerator is used 
+    if(number1<number2)//onditional loop is implement where you can see that opoerator is used 
 		{
-			System.out.println(num1+ "number 1 is larger");
+			System.out.println("number 1 is larger" +number1);
 		}
 		else//condition false than this loop will applied
 		{
-			System.out.println(num2+ "number 2 is larger");
-
-		}
-		
+			System.out.println("number 2 is larger" +number2);
 	}
+	
+   }
+}
