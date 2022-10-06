6892eaee227cba5336d3f9f53c1539be4e1ff1fd
DeekshantBhardwaj <93893639+DeekshantBhardwaj@users.noreply.github.com>
2022-10-06 11:13:17 +0000

    Update pattern.java

--- HandsOwnprojects/Thrusday2/pattern.java
+++ HandsOwnprojects/Thrusday2/pattern.java
@@ -14,9 +14,21 @@ class ReversePyramid {
 		int size=sc.nextInt();
 		//we can use of for loop to execte this programme and after this we have seen a reverse pattren 
 		for (int i= 0; i<= size-1; i++)
+			/* this loop having the 3 conditions are there so you can taken value from the user like "5"
+			2.next inilizaton was there about(5-1)
+		                                         (then i value should be 4 now(4-1) this conditons folllows again and again and print 
+			*/
 	    {
-	      for (int j=0; j<=i; j++)//loop another loop will execute but this loop is using for only for spacing
-	      {
+	      for (int j=0; j<=i; j++)/*loop another 
+	      				loop will execute but this loop 
+	      				is using for only for spacing
+	      				this will be executed like 1.******
+								   2. ****(this follows the first
+									    taken "i" value 
+									    then come to "j" for 
+								             spacing)
+*/
+{
 	        System.out.print(" ");
 	      }
 	    
