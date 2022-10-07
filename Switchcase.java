dfbd29578f942b4097a59352810e4635cdcee17f
DeekshantBhardwaj <93893639+DeekshantBhardwaj@users.noreply.github.com>
2022-10-07 10:05:46 +0000

    Create Switchcase.java

---
+++ Switchcase.java
@@ -0,0 +1,24 @@
+/*
+WAP to demonstrate on switch case using input from the user
+Author-Deekshant bhardwaj
+Date-7/03/2022
+*/
+import java.util.Scanner;
+class Switchcase
+{
+  public static void main(String[] args)
+  {
+    Scanner sc= new Scanner(System.in);
+    System.out.println("Enter the number");
+      int number=sc.nextInt();
+    System.out.println("Enter second number");
+      int number2=sc.nextInt();
+    int adding,divide,calculator;
+    switch(calculator==1)
+    {
+      case 1:
+        adding= number+number2;
+        break;
+      case 2;
+        divide=number/number2;
+        break;
