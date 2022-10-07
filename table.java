7d50a3142cd074fd2f345f94f6782675f4e9381a
DeekshantBhardwaj <93893639+DeekshantBhardwaj@users.noreply.github.com>
2022-10-07 10:25:33 +0000

    Update table.java

--- table.java
+++ table.java
@@ -1,15 +1,17 @@
 import java.util.Scanner;
 class Table
-
-{
-public static void main(String[] args)
-{ 
-  Scanner sc= new Scanner(System.in);
-    System.out.println("enter the number :");
-   int number= sc.nextInt();
-  for(int i=0;i<=10;i++)
-  {
-    System.out.println(number+" x " +i+ " = "+number*i);
-    }
-}
-}
+{  
+    public static void main(String args[])   
+    {  
+    Scanner sc = new Scanner(System.in);  
+    System.out.print("Enter number: ");       
+    //reading a number whose table is to be print  
+    int num=sc.nextInt();  
+    //loop start execution form and execute until the condition i<=10 becomes false  or when the value of i=10
+    for(int i=1; i <= 10; i++)  
+    {  
+    //prints table of the entered number      
+    System.out.println(number+" x "+i+" = "+number*i);  
+    }  
+    }  
+    }  
