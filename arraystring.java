79d7663d8f23793423559b396eda96a7fcfd2c76
DeekshantBhardwaj <93893639+DeekshantBhardwaj@users.noreply.github.com>
2022-10-19 18:11:32 +0000

    Create arraystring.java

---
+++ arraystring.java
@@ -0,0 +1,19 @@
+/*
+Write a programme to traverse the array in string
+Author=Deekshant bhardwaj
+Date= 19/09/22
+*/
+class datatype{
+  
+    public static void main( String[] args)
+  {
+    String[] a= new String[];
+    a[0]="happy";
+    a[1]="see";
+     a[2]="you";
+    for(int i=0;i<a.length;i++)
+    {
+      System.out.println(a[3]);
+    }
+  }
+}
