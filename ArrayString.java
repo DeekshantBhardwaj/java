0e95679aa35b0d9e590f1acbf5112cd2ccbb2edd
DeekshantBhardwaj <93893639+DeekshantBhardwaj@users.noreply.github.com>
2022-10-19 09:51:27 +0000

    Create ArrayString.java

---
+++ ArrayString.java
@@ -0,0 +1,11 @@
+//Write aprogramme to take input as string and print an array
+//Author=Deekshant Bhardwaj
+//Date=
+class String
+{
+public static void main(Sting args[])
+{
+String a[]= {"21","2","3","4","5"};
+System.out.println("your array"+ a[]);
+}
+}
