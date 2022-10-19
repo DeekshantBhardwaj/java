864fc6c0b0e6eb887d47ace573a0440a5d8bf986
DeekshantBhardwaj <93893639+DeekshantBhardwaj@users.noreply.github.com>
2022-10-19 18:44:09 +0000

    Create UserINPUTarray.java

---
+++ UserINPUTarray.java
@@ -0,0 +1,18 @@
+/*
+Write a programme to give input by the usesr to print the array
+Author=Deekshant bhardwaj
+Date=19/09/22
+
+*/
+import java.utils.Scanner;
+class userInput
+{
+  public static void main9(String[] args)
+  {
+    Scanner sc= new Scanner(System.in);
+    System.out.println("Enter the number:");
+    int a=sc.nextInt();
+    int arr[]=new int[a];
+    System.out.println("the Size of the array:");
+  }
+}
