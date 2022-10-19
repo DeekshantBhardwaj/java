a80a169ea49b829a881bb6f6ffe7270ed0d545ba
DeekshantBhardwaj <93893639+DeekshantBhardwaj@users.noreply.github.com>
2022-10-19 17:57:03 +0000

    Update ArrayString.java

--- ArrayString.java
+++ ArrayString.java
@@ -1,11 +1,20 @@
-//Write aprogramme to take input as string and print an array
+//Write a programme to tgraraverse a array
 //Author=Deekshant Bhardwaj
-//Date=
-class String
+//Date=19/09/22
+class tring
 {
 public static void main(Sting args[])
 {
-String a[]= {"21","2","3","4","5"};
-System.out.println("your array"+ a[]);
+//we can declearing the array
+int a[]=new int[3]
+a[0]=12
+a[1]=13
+a[2]=14
+//inislization of thr array
+for(i=0;i<a.length;i++)
+{
+System.out.println(a[i]);
+}
+
 }
 }
